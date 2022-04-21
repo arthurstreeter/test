9142971096fc9dfefed855b5884f24951dd65d92
Arthur Streeter <arthurishere@gmail.com>
2022-04-21 00:14:45 +0000

    Milestone 2:  The Timeline Class

--- test/test_timeline.py
+++ test/test_timeline.py
@@ -7,6 +7,12 @@ import exceptions
     "events",
     [
         # TODO: add tests for edge cases
+        pytest.param("1,4 2,5", id="events are a string"),
+        pytest.param((1,4), id="events are a tuple"),
+        pytest.param(([1,4],[2,5]), id="events are a tuple of lists"),
+        pytest.param({1:[0,4],2:[5,10]}, id="events are a dict"),
+        pytest.param([Event(1,3),Event(1,4),Event(1,5)], id="events start at the same time"),
+        pytest.param([Event(1,3),Event(2,4),Event(3,5)], id="events overlap")
     ],
 )
 def test_invalid_events(events):
@@ -17,7 +23,8 @@ def test_invalid_events(events):
 @pytest.mark.parametrize(
     "start, duration",
     [
-        # TODO: add tests for edge cases
+        pytest.param(21,1, id="event's start is beyond Timeline.size"),
+        pytest.param(1,50, id="event's duration is longer than Timeline.size"),
     ],
 )
 def test_out_of_bounds(start, duration):
@@ -29,6 +36,8 @@ def test_out_of_bounds(start, duration):
     "events, expected",
     [
         # TODO: add tests for edge cases
+        pytest.param([Event(0,4),Event(4,1),Event(5,5)], {"min":0,"max":5,"percent":.5}, id="events are next to eachother"),
+        pytest.param([Event(9,1),Event(13,2),Event(16,3)], {"min":9,"max":3,"percent":.3}, id="events are spaced out"),
     ],
 )
 def test_get_duration_stats(events, expected):
--- timeline.py
+++ timeline.py
@@ -1,3 +1,5 @@
+from operator import index
+import exceptions
 
 class Timeline:
     size = 20  # total size of the timeline, events may not end at or beyond this point
@@ -14,6 +16,15 @@ class Timeline:
         """
         # TODO: implement
 
+        if not isinstance(events, list):
+            raise exceptions.InvalidEventsError()
+
+        self.check_out_of_bounds(events)
+        self.check_overlapping(events)
+
+        self.events = events
+
+
     def check_out_of_bounds(self, events: list):
         """check all events in list for whether they go beyond Timeline.size
 
@@ -24,6 +35,11 @@ class Timeline:
             EventOutOfBoundsError: if event is OOB
         """
         # TODO: implement
+        for event in events:
+            end_time = event.start + event.duration
+            if end_time > Timeline.size:
+                raise exceptions.EventOutOfBoundsError()
+
 
     def check_overlapping(self, events: list):
         """raise exception if any events are overlapping
@@ -35,6 +51,22 @@ class Timeline:
             exceptions.InvalidEventsError: if events are overlapping
         """
         # TODO: implement
+        for i, event in enumerate(events):
+            end_event = event.start + event.duration
+            for k, check in enumerate(events):
+                end_check = check.start + check.duration
+                # Exclude current event in the list.
+                if i == k:
+                    continue
+                if check.start == event.start:
+                    raise exceptions.InvalidEventsError()
+                elif check.start < end_event and end_check > event.start:
+                    # Do not raise an exception if events end and start at the same index.
+                    if end_check == event.start:
+                        continue
+                    raise exceptions.InvalidEventsError()
+
+
 
     def get_duration_stats(self) -> dict:
         """calculate the minimum and maximum event durations as well as the percentage
@@ -49,6 +81,21 @@ class Timeline:
                 }
         """
         # TODO: implement
+
+        event_starts = []
+        event_durations = []
+        event_max = 0
+        for event in self.events:
+            event_starts.append(event.start)
+            event_durations.append(event.duration)
+            event_max += event.duration
+        
+        results = {
+            "min": min(event_starts),
+            "max": max(event_durations), 
+            "percent": event_max/Timeline.size
+            }
+        return results
 
     def __repr__(self) -> str:
         return f"<Timeline events={self.events}>"
